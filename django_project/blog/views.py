47ed2f28052032ca146a66ab8f80cd565bf667f0
ElaySuperCoder <36122259+Maharramli@users.noreply.github.com>
2020-05-02 23:36:57 +0400

    Part 3

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,9 +1,28 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+posts = [
+    {
+        'author': 'Elay Maharramli',
+        'title': 'Blog Post 1',
+        'content':'First post content',
+        'date_posted':'March 2, 2020'
+    },
+    {
+        'author': 'Jane Doe',
+        'title': 'Blog Post 2',
+        'content':'Second post content',
+        'date_posted':'March 3, 2020'
+    },
+]
+
+
 
 def home(request):
-    return HttpResponse('<h1>Blog Home</h1>')
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
 
 def about(request):
-    return HttpResponse('<h1>Blog About</h1>')
+    return render(request, 'blog/about.html', {'title': 'About'})
